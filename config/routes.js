46d4beffb9ff9eb49f8b49fc8900706b78c39cbe
aupreti1 <anaupreti@gmail.com>
2019-07-01 23:54:08 -0400

    added an update for headlines and get for notes

--- config/routes.js
+++ config/routes.js
@@ -46,4 +46,21 @@ module.exports = function(router) {
             res.json(data);
         });
     });
+
+    router.patch("/api/headlines", function(req, res) {
+        headlinesController.update(req.body, function(err, data) {
+            res.json(data);
+        });
+    });
+
+    router.get("/api/notes/:headline_id?", function(req, res) {
+        var query = {};
+        if (req.prams.headlines_id) {
+            query._id = req.params.headline_id;
+        }
+
+        notesController.get(query, function(err, data) {
+            res.json(data);
+        });
+    });
 }
