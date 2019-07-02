35e61374724eedd9887b4872b9d292a2674a31d9
aupreti1 <anaupreti@gmail.com>
2019-07-02 01:01:29 -0400

    Working on saved.js

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -8,5 +8,22 @@ $(document).ready(function() {
 
     initPage();
 
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=true").then(function(data) {
+            if (data && data.length) {
+                renderArticles(data);
+            }
+            else {
+                renderEmpty();
+            }
+        });
+    }
     
+    function renderArticles(articles) {
+        var articlePanels = [];
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+    }
 })
