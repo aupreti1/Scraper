6f78ea2bc563a1dbe2ddebbf0b9acd1428bc01ed
aupreti1 <anaupreti@gmail.com>
2019-07-02 00:21:30 -0400

    started on the index.js

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    var articleContainer = $(".article-container");
+    $(document).on("click", "btn.save", handleArticleSave);
+    $(document).on("click", ".scrape-new", handleArticleScrape);
+
+    initPage();
+
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=false")
+            .then(function(data) {
+                if (data && data.length) {
+                    renderArticles(data);
+                }
+                else {
+                    renderEmpty();
+                }
+            });
+    }
+
+    function renderArticles(articles) {
+        var articlePanels = [];
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+
+        articleContainer.append(articlePanels);
+    }
+
+    function creatPanel(article) {
+        var panel = 
+            $(["<div class='panel panel-default'>",
+                "<div class='panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class='btn btn-success save'>",
+                "Save Article",
+                "</a>",
+                "</h3>",
+                "</div>",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"]
+                .join(""));
+    }
+})
