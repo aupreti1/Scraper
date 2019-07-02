9ccfd0675800feba20d09bb7a0059be3f3c53312
aupreti1 <anaupreti@gmail.com>
2019-07-01 23:18:48 -0400

    added a fetch api route on routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,7 @@
+var scrape = require("../scripts/scrape");
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router) {
     // Route for home
     router.get("/", function(req, res) {
@@ -7,5 +11,20 @@ module.exports = function(router) {
     // Route for Saved Articles
     router.get("/saved", function(req, res) {
         res.render("saved");
+    });
+
+    router.get("/api/fetch", function(req, res) {
+        headlinesController.fetch(function(err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No New Articals Today!"
+                });
+            }
+            else {
+                res.json({
+                    message: "Added " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
     });
 }
